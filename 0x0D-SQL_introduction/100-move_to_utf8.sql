f26cd79ec3465525327e918190dad771791c87e1
isaac <106970737+IsaacAbraham51@users.noreply.github.com>
2022-11-30 01:46:24 +0300

    Databases

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,5 @@
--- Lists all records of this table
+-- converts 'hbtn_0c_0' database to UTF8
 
-SELECT score, name FROM second_table ORDER BY score DESC;
+USE hbtn_0c_0;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
