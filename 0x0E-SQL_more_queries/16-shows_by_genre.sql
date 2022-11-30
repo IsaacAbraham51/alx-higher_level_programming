b56121625b6e5a57303359c3f17df55102302407
isaac <106970737+IsaacAbraham51@users.noreply.github.com>
2022-12-01 00:10:46 +0300

    Databases

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,8 +3,11 @@
 -- Each record should display tv_shows.title, tv_genres.name
 -- Results must be sorted in ascending order by show title
 -- You can only use one SELECT statement
-SELECT s.title, g.name
-FROM tv_shows s
-LEFT JOIN tv_show_genres m ON s.id = m.show_id
-LEFT JOIN tv_genres g ON m.genre_id = g.id
-ORDER BY s.title ASC;
+
+SELECT a.title, c.name
+FROM tv_shows AS a
+LEFT JOIN tv_show_genres AS b
+ON a.id = b.show_id
+LEFT JOIN tv_genres AS c
+ON b.genre_id = c.id
+ORDER BY a.title ASC, c.name ASC;
